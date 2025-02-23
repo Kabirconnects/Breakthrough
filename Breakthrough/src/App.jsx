b8a28398689222399ed51644db17f75acbb83f13
Kabirconnects <himanshuarodia5@gmail.com>
2025-02-23 23:50:11 +0530

    Breakthrough/src/App.jsx

--- Breakthrough/src/App.jsx
+++ Breakthrough/src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from './components';
 
 function App() {
-  return (
-    <div>
-      
+  const [loading, setLoading] = useState(true); 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false); 
+      });
+  }, [dispatch]); 
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-amber-600">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Todo: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
     </div>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
